64ffdf37412d4b7acac4ae701a8f1acc4e18e6a2
Ralph Lauren <ralphlauren114@gmail.com>
2024-05-09 07:03:40 +0800

    Added starting UI elements

--- script.js
+++ script.js
@@ -26,5 +26,4 @@ const navigation = document.querySelector('.navigation');
 document.querySelector('.toggle').onclick = function () {
     navigation.classList.toggle('active');
 
-    console.log('active');
 }
