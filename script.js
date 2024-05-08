12ff4ab75e4a17892cc5bbe6adbfa3c250c9cffd
Ralph Lauren <ralphlauren114@gmail.com>
2024-05-08 12:15:17 +0800

    Added #Tabs and #Content

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const navigation = document.querySelector('.navigation');
+
 document.querySelector('.toggle').onclick = function () {
     navigation.classList.toggle('active');
+}
+
+function switchContent(content) {
+    // Hide all content divs
+    document.getElementById('search-content').style.display = 'none';
+    document.getElementById('home-content').style.display = 'none';
+    document.getElementById('about-content').style.display = 'none';
+    document.getElementById('download-content').style.display = 'none';
+    document.getElementById('admin-content').style.display = 'none';
+
+    // Show the selected content div
+    document.getElementById(content + '-content').style.display = 'block';
 }
